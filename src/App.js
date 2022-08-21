16aaebe96751ecdac3f4afbb02c088259045370f
Shaun Prince <shaun@techfusion.ca>
2022-08-21 01:17:47 -0700

    nothihng to see here

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Nothing to see here yet...
         </p>
         <a
           className="App-link"
